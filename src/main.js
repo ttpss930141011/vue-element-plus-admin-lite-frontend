59a710261f58a5493681135833e4a07f713917fa
Justin Xiao <o927416847@gmail.com>
2022-06-07 17:02:38 +0800

    [add] install vue-good-table-next & try do in index page

--- src/main.js
+++ src/main.js
@@ -8,16 +8,27 @@ import 'element-plus/dist/index.css'
 import '@/router/permission'
 import SvgIcon from '@/components/SvgIcon'
 import DataTable from './views/Index'
+import VueGoodTablePlugin from 'vue-good-table-next'
+import 'vue-good-table-next/dist/vue-good-table-next.css'
+
 // main.js
-import { Expand, ChatRound, ArrowDown, CaretBottom, Close, RefreshLeft } from '@element-plus/icons-vue'
-import '@/icons'// svg component
+import {
+  Expand,
+  ChatRound,
+  ArrowDown,
+  CaretBottom,
+  Close,
+  RefreshLeft
+} from '@element-plus/icons-vue'
+import '@/icons' // svg component
 window.onerror = function (message, source, lineno, colno, error) {
   console.log(message, source, lineno, colno, error)
 } // icon
 const app = createApp(App)
 // 第一个参数传入的组件名即是之后调用时使用的组件名
 // 此处组件名格式参照的时官方文档中icon部分的用例格式
-app.component('expand', Expand)
+app
+  .component('expand', Expand)
   .component('chat-round', ChatRound)
   .component('arrow-down', ArrowDown)
   .component('caret-bottom', CaretBottom)
@@ -32,4 +43,9 @@ app.component('expand', Expand)
 
 app.component('svg-icon', SvgIcon)
 
-app.use(store).use(ElementPlus).use(router).mount('#app')
+app
+  .use(store)
+  .use(ElementPlus)
+  .use(router)
+  .use(VueGoodTablePlugin)
+  .mount('#app')
